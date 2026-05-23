002546f0b1e5508157f9d6bbb0022cff92a1426c
Caio de Oliveira <oliveira.gctt@gmail.com>
2026-05-23 15:22:23 -0300

    testes até final de seção 5

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -1,6 +1,6 @@
 describe('Formulario de Consultoria', () => {
 
-    it('Deve solicitar consultoria individual', () => {
+    it.only('Deve solicitar consultoria individual', () => {
         cy.start()
         cy.submitLogin('papito@webdojo.com', 'katana123')
 
@@ -50,13 +50,13 @@ describe('Formulario de Consultoria', () => {
         })
 
         cy.get('input[type="file"]') // Upload de arquivo
-            .selectFile('./cypress/fixtures/document.pdf', {force : true})
+            .selectFile('./cypress/fixtures/document.pdf', { force: true })
 
 
         cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]') //Campo de texto grande
             .type('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
 
-        
+
         const techs = [
             'Cypress',
             'Selenium',
@@ -84,14 +84,53 @@ describe('Formulario de Consultoria', () => {
         cy.contains('button', 'Enviar formulário')
             .click()
 
-        cy.contains('Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
+        cy.get('.modal', {timeout: 7000})
             .should('be.visible')
+            .find('.modal-content')
+            .should('be.visible')
+            .and('have.text', 'Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
+
 
         cy.contains('button', 'Fechar')
             .click()
 
+    })
+
+    it('Deve verificar os campos obrigatórios', () => {
+        cy.start()
+        cy.submitLogin('papito@webdojo.com', 'katana123')
+
+        cy.goTo('Formulários', 'Consultoria')
+
+        cy.contains('button', 'Enviar formulário')
+            .click()
 
 
+        cy.contains('label', 'Nome Completo')
+            .parent()
+            .find('p')
+            .should('be.visible')
+            .should('have.text', 'Campo obrigatório')
+            .and('have.class', 'text-red-400')
+            .and('have.css', 'color', 'rgb(248, 113, 113)')
+
+        cy.contains('label', 'Email')
+            .parent()
+            .find('p')
+            .should('be.visible')
+            .should('have.text', 'Campo obrigatório')
+            .and('have.class', 'text-red-400')
+            .and('have.css', 'color', 'rgb(248, 113, 113)')
+        
+        cy.contains('label', 'termos de uso')
+            .parent()
+            .find('p')
+            .should('be.visible')
+            .should('have.text', 'Você precisa aceitar os termos de uso')
+            .and('have.class', 'text-red-400')
+            .and('have.css', 'color', 'rgb(248, 113, 113)')
+
+        
     })
 })
 
