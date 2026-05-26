3ea37c420434b641be559a95b7761d7d85ff4de3
Caio de Oliveira <oliveira.gctt@gmail.com>
2026-05-25 21:42:52 -0300

    final da aula 43

---
+++ web/cypress/e2e/iframe.cy.js
@@ -0,0 +1,22 @@
+describe('iFrame', () => {
+    it('Tocar vídeo de fundo', () => {
+        cy.start()
+        cy.submitLogin('papito@webdojo.com', 'katana123')
+        cy.contains('Video').click()
+
+        cy.get('iframe[title="Video Player"]') //resumo da função é que ele pega o conteúdo do body do iframe e transforma em elemento do cypress para usar os comandos
+            .should('exist')
+            .its('0.contentDocument.body') //função usada para pegar propriedades de elementos
+            .then(cy.wrap) //wrap pega valor ou elemento e transforma em objeto cypress 
+            .as('iFramePlayer') 
+
+        cy.get('@iFramePlayer')
+            .find('.play-button')
+            .click()
+
+        cy.get('@iFramePlayer')
+            .find('.pause-button')
+            .should('be.visible')
+
+    })
+})
